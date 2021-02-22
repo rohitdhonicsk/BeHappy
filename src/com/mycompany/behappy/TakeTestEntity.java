4999f7b2e4ee01d4297edf2edcf1184de6f606fb
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2021-02-22 21:16:48 +0530

    Added the edited files

---
+++ src/com/mycompany/behappy/TakeTestEntity.java
@@ -0,0 +1,140 @@
+package taketest;
+
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException;  
+public class TakeTestEntity 
+{
+    private int StressScore;
+    private int AnxietyScore;
+    private int DepressionScore;
+    private String feedback;
+
+    public TakeTestEntity() {
+        this.StressScore = 0;
+        this.AnxietyScore = 0;
+        this.DepressionScore = 0;
+        this.feedback = "";
+        
+        
+    }
+
+    public int getStressScore() {
+        return StressScore;
+    }
+
+    public int getAnxietyScore() {
+        return AnxietyScore;
+    }
+
+    public int getDepressionScore() {
+        return DepressionScore;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setStressScore(int StressScore) {
+        this.StressScore = StressScore;
+    }
+
+    public void setAnxietyScore(int AnxietyScore) {
+        this.AnxietyScore = AnxietyScore;
+    }
+
+    public void setDepressionScore(int DepressionScore) {
+        this.DepressionScore = DepressionScore;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+    
+    
+}
+/*{
+    public String StressQuestions[][] = new String[9][7];
+    public String headersStress[] = new String[7];
+    public String AnxietyQuestions[][] = new String[8][6];
+    public String headersAnxiety[] = new String[6];
+    public String DepressionQuestions[][] = new String[21][6];
+    public String headersDepression[] = new String[6];
+
+    void Stress()
+    {
+        String line = "";  
+        String splitBy = ",";
+        
+        int c = 0;
+        try   
+        {  
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("C://Users/Aanisha/Desktop/TakeTest/utils/StressTest.csv"));  
+            while ((line = br.readLine()) != null)   //returns a Boolean value  
+            {  
+                if(c==0)
+                    headersStress = line.split(splitBy);
+                else
+                    StressQuestions[c-1] = line.split(splitBy);    
+
+                c++;
+            }  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }
+    }
+
+    void Anxiety()
+    {
+        String line = "";  
+        String splitBy = ",";
+        int c = 0;
+        try   
+        {  
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("C://Users/Aanisha/Desktop/TakeTest/utils/AnxietyTest.csv"));  
+            while ((line = br.readLine()) != null)   //returns a Boolean value  
+            {  
+                if(c==0)
+                    headersAnxiety = line.split(splitBy);
+                else
+                    AnxietyQuestions[c-1] = line.split(splitBy);    
+
+                c++;
+            }  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }
+    }
+    void Depression()
+    {
+        String line = "";  
+        String splitBy = ",";
+        int c = 0;
+        try   
+        {  
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("C://Users/Aanisha/Desktop/TakeTest/utils/"
+                    + "DepressionTest.csv"));  
+            while ((line = br.readLine()) != null)   //returns a Boolean value  
+            {  
+                if(c==0)
+                    headersDepression = line.split(splitBy);
+                else
+                    DepressionQuestions[c-1] = line.split(splitBy);    
+
+                c++;
+            }  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }
+    }
+
+}*/
---
+++ src/com/mycompany/behappy/TakeTestManager.java
@@ -0,0 +1,314 @@
+package taketest;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import taketest.TakeTestEntity;
+import java.util.Scanner;
+public class TakeTestManager 
+{
+    private String StressQuestions[][];
+    private String headersStress[];
+    private String AnxietyQuestions[][];
+    private String headersAnxiety[];
+    private String DepressionQuestions[][];
+    private String headersDepression[];
+    
+
+    public TakeTestManager() 
+    {
+        init();
+    }
+    public void init()
+    {
+        StressQuestions = new String[9][7];
+        headersStress = new String[7];
+        AnxietyQuestions = new String[8][6];
+        headersAnxiety  = new String[6];
+        DepressionQuestions = new String[21][6];
+        headersDepression = new String[6];
+        Stress();
+        Anxiety();
+        Depression();
+        
+    }
+    void Stress()
+    {
+        String line = "";  
+        String splitBy = ",";
+        
+        int c = 0;
+        try   
+        {  
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("C://Users/Aanisha/Desktop/TakeTest/utils/StressTest.csv"));  
+            while ((line = br.readLine()) != null)   //returns a Boolean value  
+            {  
+                if(c==0)
+                    headersStress = line.split(splitBy);
+                else
+                    StressQuestions[c-1] = line.split(splitBy);    
+
+                c++;
+            }  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }
+    }
+
+    void Anxiety()
+    {
+        String line = "";  
+        String splitBy = ",";
+        int c = 0;
+        try   
+        {  
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("C://Users/Aanisha/Desktop/TakeTest/utils/AnxietyTest.csv"));  
+            while ((line = br.readLine()) != null)   //returns a Boolean value  
+            {  
+                if(c==0)
+                    headersAnxiety = line.split(splitBy);
+                else
+                    AnxietyQuestions[c-1] = line.split(splitBy);    
+
+                c++;
+            }  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }
+    }
+    void Depression()
+    {
+        String line = "";  
+        String splitBy = ",";
+        int c = 0;
+        try   
+        {  
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("C://Users/Aanisha/Desktop/TakeTest/utils/"
+                    + "DepressionTest.csv"));  
+            while ((line = br.readLine()) != null)   //returns a Boolean value  
+            {  
+                if(c==0)
+                    headersDepression = line.split(splitBy);
+                else
+                    DepressionQuestions[c-1] = line.split(splitBy);    
+
+                c++;
+            }  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }
+    }
+    public String getStress(int i)
+    {
+        return StressQuestions[i][1];
+        
+    }
+     public String getAnxiety(int i)
+    {
+        return AnxietyQuestions[i][1];
+        
+    }
+     public String getDepression(int i)
+     {
+         return DepressionQuestions[i][1];
+     }
+     public String feedback(int StressScore, int DepressionScore,int AnxietyScore )
+     {
+        String opn = "";
+        if(StressScore>=0 && StressScore<=10)
+            opn+="You don't have any stress related disorder,";
+        else if(StressScore>=11 && StressScore<=16)
+            opn+="You are likely to have a mild stress disorder,";
+
+        else if(StressScore>=16 && StressScore<=20)
+            opn+="You are likely to have a moderate stress disorder,";
+        else if(StressScore>=20)
+            opn+="You are likely to have severe stress disorder,";
+
+        String op="";
+        if(DepressionScore>=0 && DepressionScore<=10)
+                op+="and minimal depression";
+        else if(DepressionScore>=11 && DepressionScore<=16)
+                op+="and mild depression";
+        else if(DepressionScore>=16 && DepressionScore<=20)
+                op+="and moderate depression";
+        else
+                op+="and severe depression";
+
+
+        String opA = "";
+        if(AnxietyScore<10)
+            opA+="mild anxiety ";
+        else if(AnxietyScore>=11 && AnxietyScore < 16)
+            opA+="moderate anxiety ";
+        else
+            opA+="severe anxiety ";
+
+        return opn+opA+op;
+        //System.out.println(feedback);
+    }
+    
+    
+}
+/*{
+    public int StressScore;
+    public int AnxietyScore;
+    public int DepressionScore;
+    public String feedback;
+    
+    public static TakeTestEntity ent = new TakeTestEntity();
+    Scanner sc = new Scanner(System.in);
+    public String feedback()
+    {
+        String opn = "";
+        if(StressScore>=0 && StressScore<=19)
+            opn+="You don't have any stress related disorder,";
+        else if(StressScore>=20 && StressScore<=24)
+            opn+="You are likely to have a mild stress disorder,";
+
+        else if(StressScore>=25 && StressScore<=29)
+            opn+="You are likely to have a moderate stress disorder,";
+        else if(StressScore>=30 && StressScore<=50)
+            opn+="You are likely to have severe stress disorder,";
+
+        String op="";
+        if(DepressionScore>=0 && DepressionScore<=10)
+                op+="and minimal depression";
+        else if(DepressionScore>=11 && DepressionScore<=20)
+                op+="and mild depression";
+        else if(DepressionScore>=21 && DepressionScore<=29)
+                op+="and moderate depression";
+        else
+                op+="and severe depression";
+
+
+        String opA = "";
+        if(AnxietyScore<18)
+            opA+="mild anxiety ";
+        else if(AnxietyScore>=18)
+            opA+="moderate anxiety ";
+        else
+            opA+="severe anxiety ";
+
+        feedback = opn+opA+op;
+        System.out.println(feedback);
+    }
+    public void StressScore()
+    {
+        //for(int i = 0;i<9;i++)
+        //{
+            /*System.out.println(StressQuestions[i][0]+" In the past 4 weeks,"+StressQuestions[i][1]);
+            System.out.println("Press 1 for op1, 2 for op2, 3 for op3, 4 for op4, 5 for op5");
+            System.out.println(StressQuestions[i][2]);
+            System.out.println(StressQuestions[i][3]);
+            System.out.println(StressQuestions[i][4]);
+            System.out.println(StressQuestions[i][5]);
+            System.out.println(StressQuestions[i][6]);*/
+            /*int a = 1;
+
+            while(a==1)
+            {
+                int op = sc.nextInt();
+                if(op<1 || op>5)
+                 System.out.println("Press 1 for op1, 2 for op2, 3 for op3, 4 for op4, 5 for op5");
+                else
+                {
+                    if(op==1)
+                     StressScore+=5;
+                    else if(op==2)
+                     StressScore+=4;
+                    else if(op==3)
+                     StressScore+=3;
+                    else if(op==4)
+                     StressScore+=2;
+                    else
+                     StressScore+=1;
+
+                    a = 2;
+                    
+                    
+
+                }
+            }*/
+
+
+        //}
+       /* ent.Stress();
+        ent.Anxiety();
+        ent.Depression();
+        //AnxietyScore();
+        
+    }
+    public String getStress(int i)
+    {
+        return ent.StressQuestions[i][1];
+        
+    }
+     public String getAnxiety(int i)
+    {
+        return ent.AnxietyQuestions[i][1];
+        
+    }
+     public String getDepression(int i)
+     {
+         return ent.DepressionQuestions[i][1];
+     }
+    public void DepressionScore()
+    {
+        for(int i = 0;i<21;i++)
+        {
+            System.out.println(ent.DepressionQuestions[i][0]+" In the past 2 weeks, how often did you have this feeling : "+ent.DepressionQuestions[i][1]);
+            System.out.println("Press 1 for op1, 2 for op2, 3 for op3, 4 for op4");
+            System.out.println(ent.DepressionQuestions[i][2]);
+            System.out.println(ent.DepressionQuestions[i][3]);
+            System.out.println(ent.DepressionQuestions[i][4]);
+            System.out.println(ent.DepressionQuestions[i][5]);
+            
+            int a = 1;
+
+            while(a==1)
+            {
+                int op = sc.nextInt();
+                if(op<1 || op>5)
+                 System.out.println("Press 1 for op1, 2 for op2, 3 for op3, 4 for op4");
+                else
+                {
+                    if(op==1)
+                     DepressionScore+=3;
+                    else if(op==2)
+                     DepressionScore+=2;
+                    else if(op==3)
+                     DepressionScore+=1;
+                    else if(op==4)
+                     DepressionScore+=0;
+                    
+
+                    a = 2;
+                    
+                    
+
+                }
+            }
+
+
+        }
+        feedback();
+    }
+    public static void main(String args[])
+    {
+        TakeTestManager test = new TakeTestManager();
+        
+        test.StressScore();
+        
+        
+    }
+}*/
